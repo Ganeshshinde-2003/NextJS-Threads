f63e0b11278e8ada1579cb76e217fbaef5664f19
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-09-01 11:19:13 +0530

    added the comments

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,24 +1,24 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { usePathname } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
+} from "@/components/ui/form"; 
+
 import { Input } from "../ui/input";
-import { updateUser } from "@/lib/actions/user.actions";
-import { usePathname, useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+
 import { CommentValidation } from "@/lib/validations/thread";
-import { createThread } from "@/lib/actions/thread.actions";
-import Image from "next/image";
+import { addCommentToThread } from "@/lib/actions/thread.actions";
 
 interface Props {
   threadId: string;
@@ -27,55 +27,60 @@ interface Props {
 }
 
 function Comment({ threadId, currentUserImg, currentUserId }: Props) {
-    const router = useRouter();
-    const pathname = usePathname();
-  
-    const form = useForm({
-      resolver: zodResolver(CommentValidation),
-      defaultValues: {
-        thread: "",
-      },
-    });
-  
-    const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
-    //   await createThread(
-    //     {
-    //       text: values.thread,
-    //       communityId: null,
-    //       path: pathname,
-    //     }
-    //   );
-  
-      router.push("/");
-    }
-  return (
-    <div>
-      <Form {...form}>
-        <form
-          className="comment-form"
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
-          <FormField
-            control={form.control}
-            name="thread"
-            render={({ field }) => (
-              <FormItem className="flex items-center gap-3 w-full">
-                <FormLabel className="text-base-semibold text-light-2">
-                  <Image src={currentUserImg} alt="userImage" width={48} height={48} className="rounded-full object-cover" />
-                </FormLabel>
-                <FormControl className="border-none bg-transparent">
-                  <Input type="text" placeholder="Comment..." className="no-focus text-light-1 outline-none" {...field} />
-                </FormControl>
-              </FormItem>
-            )}
-          />
+  const pathname = usePathname();
 
-          <Button type="submit" className="bg-primary-500">
-            Post Thread
-          </Button>
-        </form>
-      </Form>
-    </div>
+  const form = useForm<z.infer<typeof CommentValidation>>({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    await addCommentToThread(
+      threadId,
+      values.thread,
+      JSON.parse(currentUserId),
+      pathname
+    );
+
+    form.reset();
+  };
+
+  return (
+    <Form {...form}>
+      <form className='comment-form' onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name='thread'
+          render={({ field }) => (
+            <FormItem className='flex w-full items-center gap-3'>
+              <FormLabel>
+                <Image
+                  src={currentUserImg}
+                  alt='current_user'
+                  width={48}
+                  height={48}
+                  className='rounded-full object-cover'
+                />
+              </FormLabel>
+              <FormControl className='border-none bg-transparent'>
+                <Input
+                  type='text'
+                  {...field}
+                  placeholder='Comment...'
+                  className='no-focus text-light-1 outline-none'
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button type='submit' className='comment-form_btn'>
+          Reply
+        </Button>
+      </form>
+    </Form>
   );
 }
 
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -99,3 +99,42 @@ export async function fetchThreadById(id: string) {
     throw new Error(`Error fetching thread: ${error.message}`);
   }
 }
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string,
+) {
+  connectToDB();
+
+  try {
+   // Find the original thread by its ID
+   const originalThread = await Thread.findById(threadId);
+
+   if (!originalThread) {
+     throw new Error("Thread not found");
+   }
+
+   // Create the new comment thread
+   const commentThread = new Thread({
+     text: commentText,
+     author: userId,
+     parentId: threadId, // Set the parentId to the original thread's ID
+   });
+
+   // Save the comment thread to the database
+   const savedCommentThread = await commentThread.save();
+
+   // Add the comment thread's ID to the original thread's children array
+   originalThread.children.push(savedCommentThread._id);
+
+   // Save the updated original thread to the database
+   await originalThread.save();
+
+   revalidatePath(path);
+   
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`);
+  }
+}
