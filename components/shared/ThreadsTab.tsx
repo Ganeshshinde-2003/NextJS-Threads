6623ab51a465dd3a9b6b26a76d6662a95ddb1cb3
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-09-02 12:06:55 +0530

    created the profile page

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,15 @@
+interface Props {
+    currentUserId: string,
+    accountId: string,
+    accountType: string,
+}
+
+const ThreadsTab = async ({currentUserId, accountId, accountType} :Props) => {
+    return(
+        <section>
+            ThreadsTab
+        </section>
+    )
+}
+
+export default ThreadsTab
