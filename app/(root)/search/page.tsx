ad453672905f972e1981978421069fcf9692a9f6
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-09-05 22:31:49 +0530

    seaarch page

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,26 +1,51 @@
-import Image from "next/image";
-import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
 
-import { profileTabs } from "@/constants";
+import UserCard from "@/components/cards/UserCard";
 
-import ThreadsTab from "@/components/shared/ThreadsTab";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 
-import { fetchUser } from "@/lib/actions/user.actions";
-
-async function Page({ params }: { params: { id: string } }) {
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
   if (!user) return null;
 
-  const userInfo = await fetchUser(params.id);
-  // if (!userInfo?.onboarded) {
-  //    return redirect("/onboarding");
-  // }
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
+
   return (
     <section>
-      <h1 className="head-text mb-10">Search</h1>
+      <h1 className='head-text mb-10'>Search</h1>
+
+
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.users.length === 0 ? (
+          <p className='no-result'>No Result</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType='User'
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 }
